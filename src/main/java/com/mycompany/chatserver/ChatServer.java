207dad96b205ecd4fd99df288a43d81e1c5f03db
Mohammed-Alhilaly <mohammedalhilalyai@gmail.com>
2025-01-31 22:28:20 +0300

    Initial commit

---
+++ src/main/java/com/mycompany/chatserver/ChatServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatserver;
+
+/**
+ *
+ * @author Al-hlaly
+ */
+public class Chatserver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
